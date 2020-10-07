307804f70817b28e6ca5c2b0fb9ea4f50d933d9c
Natalia <wasiluk.natalia@gmail.com>
2020-10-07 16:11:24 +0200

    New component for Tracker Archive tasks - not editable

--- src/components/TrackerArchive.js
+++ src/components/TrackerArchive.js
@@ -5,34 +5,11 @@ export const TrackerArchive = ({ task, index, methodToEdit }) => {
 
     const [taskToShow, setTask] = useState(task)
     const [daysArr, setDayStatus] = useState([...task.daysArray])
-
-    const handleStatusChange = (element, index) => {
-        let helpArr = daysArr;
-        if (element === 1) {
-            helpArr[index] = 2;
-            setDayStatus([...helpArr])
-        } else if (element === 2) {
-            helpArr[index] = 3;
-            setDayStatus([...helpArr])
-        } else {
-            helpArr[index] = 1;
-            setDayStatus([...helpArr])
-        }
-        setTask({
-            ...taskToShow,
-            daysArray: daysArr
-        })
-    }
-
-    useEffect(() => {
-        methodToEdit(taskToShow, index);
-    }, [daysArr])
-
-    let today = new Date().getDate();
+    console.log(daysArr);
 
     return (
         <TrackerMonthlyStyled>
-            <p className='task-date'>{taskToShow.month} {taskToShow.year}</p>
+            <p className='task-date'>{taskToShow.month} {taskToShow.year} / ARCHIWALNE</p>
             <header className='task-title'>{taskToShow.title}</header>
             <div className='task-calendar'>
                 <div className='task-calendar-days-names'>
@@ -47,39 +24,34 @@ export const TrackerArchive = ({ task, index, methodToEdit }) => {
 
                 <div className='task-container___calendar'>
                     {daysArr.map((el, index) => {
-
                         if (el === 0) {
-                            return <div key={index} className={'single-day'}></div>
+                            return <div
+                                key={index}
+                                className={'single-day'}
+                            />
                         } else if (el === 1) {
-                            if (index < today + taskToShow.startingDay) {
-                                return <div
-                                    key={index}
-                                    className='single-day habit-to-be-checked'
-                                    onClick={() => handleStatusChange(el, index)}
-                                >
-                                    {index - task.startingDay + 1}
-                                </div>
-                            } else {
-                                return <div key={index}
-                                    className='un-active'
-                                >
-                                    {index - task.startingDay + 1}
-                                </div>
-                            }
+                            return <div
+                                key={index}
+                                className='single-day habit-to-be-checked'
+                            >
+                                {index - task.startingDay + 1}
+                            </div>
                         } else if (el === 2) {
-                            return <div key={index}
+                            return <div
+                                key={index}
                                 className='single-day habit-not-done'
-                                onClick={() => handleStatusChange(el, index)}>
+                            >
                             </div>
                         } else {
-                            return <div key={index}
+                            return <div
+                                key={index}
                                 className='single-day habit-done'
-                                onClick={() => handleStatusChange(el, index)}>
+                            >
                             </div>
                         }
                     })}
                 </div>
             </div>
-        </TrackerMonthlyStyled>
+        </TrackerMonthlyStyled >
     )
 }
